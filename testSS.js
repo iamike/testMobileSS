3387ba5e8ea8f376434af754b1b4242dfe2e5e73
iamike <dai.mike@gmail.com>
2015-09-16 22:00:57 +0800

    update

--- testSS.js
+++ testSS.js
@@ -17,156 +17,16 @@ lib.properties = {
 
 
 
-(lib.Juice_one_00001 = function() {
+(lib.Juice_one_00011 = function() {
 	this.spriteSheet = ss["testSS_atlas_"];
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.Juice_one_00003 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(1);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00005 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(2);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00007 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(3);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00009 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(4);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00011 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(5);
-}).prototype = p = new cjs.Sprite();
-
-
-
 (lib.Juice_one_00013 = function() {
 	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(6);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00015 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(7);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00017 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(8);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00019 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(9);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00021 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(10);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00023 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(11);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00025 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(12);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00027 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(13);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00028 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(14);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00029 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(15);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00030 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(16);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00035 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(17);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00036 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(18);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00039 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(19);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00043 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(20);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.Juice_one_00047 = function() {
-	this.spriteSheet = ss["testSS_atlas_"];
-	this.gotoAndStop(21);
+	this.gotoAndStop(1);
 }).prototype = p = new cjs.Sprite();
 
 
@@ -175,99 +35,17 @@ lib.properties = {
 (lib.testSS = function(mode,startPosition,loop) {
 	this.initialize(mode,startPosition,loop,{});
 
-	// Layer 1
-	this.shape = new cjs.Shape();
-	this.shape.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],21), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape.setTransform(287,232);
+	// Layer 2
+	this.instance = new lib.Juice_one_00013();
+	this.instance.setTransform(-8,-320);
 
-	this.shape_1 = new cjs.Shape();
-	this.shape_1.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],20), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_1.setTransform(287,232);
+	this.instance_1 = new lib.Juice_one_00011();
+	this.instance_1.setTransform(-8,-320);
 
-	this.shape_2 = new cjs.Shape();
-	this.shape_2.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],19), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_2.setTransform(287,232);
-
-	this.shape_3 = new cjs.Shape();
-	this.shape_3.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],18), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_3.setTransform(287,232);
-
-	this.shape_4 = new cjs.Shape();
-	this.shape_4.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],17), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_4.setTransform(287,232);
-
-	this.shape_5 = new cjs.Shape();
-	this.shape_5.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],16), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_5.setTransform(287,232);
-
-	this.shape_6 = new cjs.Shape();
-	this.shape_6.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],15), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_6.setTransform(287,232);
-
-	this.shape_7 = new cjs.Shape();
-	this.shape_7.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],14), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_7.setTransform(287,232);
-
-	this.shape_8 = new cjs.Shape();
-	this.shape_8.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],13), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_8.setTransform(287,232);
-
-	this.shape_9 = new cjs.Shape();
-	this.shape_9.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],12), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_9.setTransform(287,232);
-
-	this.shape_10 = new cjs.Shape();
-	this.shape_10.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],11), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_10.setTransform(287,232);
-
-	this.shape_11 = new cjs.Shape();
-	this.shape_11.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],10), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_11.setTransform(287,232);
-
-	this.shape_12 = new cjs.Shape();
-	this.shape_12.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],9), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_12.setTransform(287,232);
-
-	this.shape_13 = new cjs.Shape();
-	this.shape_13.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],8), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_13.setTransform(287,232);
-
-	this.shape_14 = new cjs.Shape();
-	this.shape_14.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],7), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_14.setTransform(287,232);
-
-	this.shape_15 = new cjs.Shape();
-	this.shape_15.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],6), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_15.setTransform(287,232);
-
-	this.shape_16 = new cjs.Shape();
-	this.shape_16.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],5), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_16.setTransform(287,232);
-
-	this.shape_17 = new cjs.Shape();
-	this.shape_17.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],4), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_17.setTransform(287,232);
-
-	this.shape_18 = new cjs.Shape();
-	this.shape_18.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],3), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_18.setTransform(287,232);
-
-	this.shape_19 = new cjs.Shape();
-	this.shape_19.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],2), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_19.setTransform(287,232);
-
-	this.shape_20 = new cjs.Shape();
-	this.shape_20.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],1), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_20.setTransform(287,232);
-
-	this.shape_21 = new cjs.Shape();
-	this.shape_21.graphics.bf(cjs.SpriteSheetUtils.extractFrame(ss["testSS_atlas_"],0), null, new cjs.Matrix2D(1,0,0,1,-320,-539)).s().p("Egx+BUOMAAAioaMBj+AAAMAAACoag");
-	this.shape_21.setTransform(287,232);
-
-	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape}]}).to({state:[{t:this.shape_1}]},1).to({state:[{t:this.shape_2}]},1).to({state:[{t:this.shape_3}]},1).to({state:[{t:this.shape_4}]},1).to({state:[{t:this.shape_5}]},1).to({state:[{t:this.shape_6}]},1).to({state:[{t:this.shape_7}]},1).to({state:[{t:this.shape_8}]},1).to({state:[{t:this.shape_9}]},1).to({state:[{t:this.shape_10}]},1).to({state:[{t:this.shape_11}]},1).to({state:[{t:this.shape_12}]},1).to({state:[{t:this.shape_13}]},1).to({state:[{t:this.shape_14}]},1).to({state:[{t:this.shape_15}]},1).to({state:[{t:this.shape_16}]},1).to({state:[{t:this.shape_17}]},1).to({state:[{t:this.shape_18}]},1).to({state:[{t:this.shape_19}]},1).to({state:[{t:this.shape_20}]},1).to({state:[{t:this.shape_21}]},1).wait(1));
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).wait(1));
 
 }).prototype = p = new cjs.MovieClip();
-p.nominalBounds = new cjs.Rectangle(242,-107,640,1078);
+p.nominalBounds = new cjs.Rectangle(267,-120,640,1078);
 
 })(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
 var lib, images, createjs, ss;
