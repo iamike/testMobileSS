3d454ddbfe7ebb578547ee1f447fca49e92645ea
iamike <dai.mike@gmail.com>
2015-09-16 17:45:13 +0800

    update

---
+++ testSS.js
@@ -0,0 +1,251 @@
+(function (lib, img, cjs, ss) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	manifest: []
+};
+
+
+
+// symbols:
+
+
+
+(lib.Juice_one_00001 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00003 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00005 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00007 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00009 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00011 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00013 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00015 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00017 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(8);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00019 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(9);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00021 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(10);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00023 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(11);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00025 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(12);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00027 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(13);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00028 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(14);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00029 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(15);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00030 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(16);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00035 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(17);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00036 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(18);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00039 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(19);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00043 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(20);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Juice_one_00047 = function() {
+	this.spriteSheet = ss["testSS_atlas_"];
+	this.gotoAndStop(21);
+}).prototype = p = new cjs.Sprite();
+
+
+
+// stage content:
+(lib.testSS = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.instance = new lib.Juice_one_00047();
+	this.instance.setTransform(-33,-307);
+
+	this.instance_1 = new lib.Juice_one_00043();
+	this.instance_1.setTransform(-33,-307);
+
+	this.instance_2 = new lib.Juice_one_00039();
+	this.instance_2.setTransform(-33,-307);
+
+	this.instance_3 = new lib.Juice_one_00036();
+	this.instance_3.setTransform(-33,-307);
+
+	this.instance_4 = new lib.Juice_one_00035();
+	this.instance_4.setTransform(-33,-307);
+
+	this.instance_5 = new lib.Juice_one_00030();
+	this.instance_5.setTransform(-33,-307);
+
+	this.instance_6 = new lib.Juice_one_00029();
+	this.instance_6.setTransform(-33,-307);
+
+	this.instance_7 = new lib.Juice_one_00028();
+	this.instance_7.setTransform(-33,-307);
+
+	this.instance_8 = new lib.Juice_one_00027();
+	this.instance_8.setTransform(-33,-307);
+
+	this.instance_9 = new lib.Juice_one_00025();
+	this.instance_9.setTransform(-33,-307);
+
+	this.instance_10 = new lib.Juice_one_00023();
+	this.instance_10.setTransform(-33,-307);
+
+	this.instance_11 = new lib.Juice_one_00021();
+	this.instance_11.setTransform(-33,-307);
+
+	this.instance_12 = new lib.Juice_one_00019();
+	this.instance_12.setTransform(-33,-307);
+
+	this.instance_13 = new lib.Juice_one_00017();
+	this.instance_13.setTransform(-33,-307);
+
+	this.instance_14 = new lib.Juice_one_00015();
+	this.instance_14.setTransform(-33,-307);
+
+	this.instance_15 = new lib.Juice_one_00013();
+	this.instance_15.setTransform(-33,-307);
+
+	this.instance_16 = new lib.Juice_one_00011();
+	this.instance_16.setTransform(-33,-307);
+
+	this.instance_17 = new lib.Juice_one_00009();
+	this.instance_17.setTransform(-33,-307);
+
+	this.instance_18 = new lib.Juice_one_00007();
+	this.instance_18.setTransform(-33,-307);
+
+	this.instance_19 = new lib.Juice_one_00005();
+	this.instance_19.setTransform(-33,-307);
+
+	this.instance_20 = new lib.Juice_one_00003();
+	this.instance_20.setTransform(-33,-307);
+
+	this.instance_21 = new lib.Juice_one_00001();
+	this.instance_21.setTransform(-33,-307);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[]}).to({state:[{t:this.instance}]},1).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).to({state:[{t:this.instance_8}]},1).to({state:[{t:this.instance_9}]},1).to({state:[{t:this.instance_10}]},1).to({state:[{t:this.instance_11}]},1).to({state:[{t:this.instance_12}]},1).to({state:[{t:this.instance_13}]},1).to({state:[{t:this.instance_14}]},1).to({state:[{t:this.instance_15}]},1).to({state:[{t:this.instance_16}]},1).to({state:[{t:this.instance_17}]},1).to({state:[{t:this.instance_18}]},1).to({state:[{t:this.instance_19}]},1).to({state:[{t:this.instance_20}]},1).to({state:[{t:this.instance_21}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = null;
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
+var lib, images, createjs, ss;
